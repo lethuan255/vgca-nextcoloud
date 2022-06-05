1d77cb3bf30bd33c8efeeec383772ee478e8e973
hoan.nguyenduy <hoanduy1710@gmail.com>
2022-06-05 16:19:42 +0700

    add code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e 
 
-git clone https://github.com/kma-custom-nextcloud/vgca-nextcloud.git
-cd vgca-nextcloud
+git clone https://github.com/kma-custom-nextcloud/vgca-nextcoloud.git
+cd vgca-nextcoloud
 cp .env.example .env
 
 composer require laravel/sail --dev -n
