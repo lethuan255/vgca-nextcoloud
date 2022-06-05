9ee94be2327f8d6cbd03fafb7e8c987b269c4ba4
hoan.nguyenduy <hoanduy1710@gmail.com>
2022-06-05 04:55:08 -0400

    change repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-git clone https://gitlab.com/nopain1210/vgca-nextcoloud.git
+git clone https://github.com/kma-custom-nextcloud/laravel-server.git
 cd vgca-nextcoloud
 cp .env.example .env
 
