6f5d6e6adcd7b61ea38456c8fda6f9d1041e571b
Lê Thuận <43976364+lethuan255@users.noreply.github.com>
2022-07-30 13:55:52 +0700

    change repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-git clone https://github.com/kma-custom-nextcloud/vgca-nextcoloud.git
+git clone https://github.com/lethuan255/vgca-nextcoloud.git
 cd vgca-nextcoloud
 cp .env.example .env
 chmod -R 775 storage
